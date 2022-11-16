1d7810f6968dcad7a90dbd2a25e46cd7eefe0b34
ihsan <hasansaygin24@gmail.com>
2022-11-16 10:55:45 +0300

    toastify added

---
+++ src/helpers/Toastify.js
@@ -0,0 +1,40 @@
+import { toast } from "react-toastify";
+
+export const toastWarnNotify = (msg) => {
+  toast.warn(msg, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const toastSuccessNotify = (msg) => {
+  toast.success(msg, {
+    position: "top-right",
+    autoClose: 3000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
+
+export const toastErrorNotify = (msg) => {
+  toast.error(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "dark",
+  });
+};
